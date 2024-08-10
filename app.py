647ca9748d6619a34f22f53e4f1666213dd84be0
ahmedfyoussef <38152165+ahmedfyoussef@users.noreply.github.com>
2024-08-10 20:30:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify , redirect
-from pptgenerator import generateppt
+from pptgeneratorr import generateppt
+
+#linko = generateppt("AI Industry")
 linko = generateppt("AI Industry")
 
 print('linko')
 print(linko)
 print('linko')
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
   #  app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
